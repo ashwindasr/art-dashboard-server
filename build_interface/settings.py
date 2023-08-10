7085c969638de579a135bb99e8e7e82e12b8f1f6
Ashwin Das <ashwindasr@gmail.com>
2023-08-10 18:02:46 -0400

    Update settings.py

--- build_interface/settings.py
+++ build_interface/settings.py
@@ -217,8 +217,6 @@ SESSION_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_DOMAIN = "art-dash.engineering.redhat.com"
-CSRF_COOKIE_DOMAIN = "art-dash.engineering.redhat.com"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
